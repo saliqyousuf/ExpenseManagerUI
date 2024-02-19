e4b14829f55609211b5a6b5f9c147ff424a39c4f
saliqyousuf <sym12384@gmail.com>
2024-02-19 19:48:33 +0530

    main changes

---
+++ expense-manager-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import AccountMenu from "@/components/menu";
+
+export default function Home() {
+  return (
+    <p>home page</p>
+  );
+}
